1fa8ad6e16e2b072486ee319c881b4efa2b4a1e0
myselfchaitanya <krishnaca@whitstack.com>
2024-09-27 00:31:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <HelmetProvider>
     <App />
     </HelmetProvider>
-  </StrictMode>,
+
 )
